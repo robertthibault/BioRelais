1741c6eaf480de88c172b92a437dc900870f9a7b
robertthibault <31767944+robertthibault@users.noreply.github.com>
2017-11-23 12:02:49 +0100

    commencement

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+require_once 'fonctions/autoload.php';
+session_start()?>
+<!DOCTYPE html>
+<html lang="fr">
+	<head>
+		<meta charset="utf-8" />
+		<title>EBioRelai</title>
+		<style type="text/css">
+			@import url(styles/EasyFood.css);
+		</style>
+	</head>
+	<body>
+		<?php
+			include 'controleurs/controleurPrincipal.php';
+		?>
+		
+	</body>
+</html>
